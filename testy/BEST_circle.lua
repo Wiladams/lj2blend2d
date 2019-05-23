1015015aaf783a9f71728ad350f9af81cf5378a8
William Adams <wiladams@microsoft.com>
2019-05-23 13:08:57 +0300

    rename STOP to BEST

--- testy/BEST_circle.lua
+++ testy/BEST_circle.lua
@@ -1,13 +1,12 @@
 
 local function app(params)
-    print("STOP_circle")
     local win1 = WMCreateWindow(params)
 
     function win1.drawForeground(self, ctxt)
         --print("win1:drawForeground(): ", self, ctxt)
         ctxt:noStroke()
         ctxt:fill(255,0,0)
-        ctxt:circle(150, 100, 75)
+        ctxt:circle(self.frame.width/2, self.frame.height/2, self.frame.height/2)
     end
 
     win1:show()
---
+++ testy/BEST_pianoapp.lua
@@ -0,0 +1,26 @@
+
+local keyboard = require("GPianoKeyboard")
+
+local function app(params)
+    local win1 = WMCreateWindow(params)
+    win1:setUseTitleBar(true)
+    
+    local kbdsets = 7
+    local kbdwidth = params.frame.width/kbdsets
+    local kbdheight = 240
+
+    -- add 5 separate instances of the keyboard
+    for i=1,kbdsets do
+        kbd = keyboard:new({frame={x= (i-1)*kbdwidth,y=0, width=kbdwidth, height=kbdheight}, scale = {x=kbdwidth, y=kbdheight}})   
+        win1:add(kbd)
+    end
+
+    win1:show()
+
+    while true do
+        win1:draw()
+        yield();
+    end
+end
+
+return app
