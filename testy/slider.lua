edf355bd574fa3d226abb79f1953417af90061dd
William Adams <wiladams@microsoft.com>
2019-05-26 13:06:37 +0300

    more work on slider

--- testy/slider.lua
+++ testy/slider.lua
@@ -2,10 +2,52 @@ local GraphicGroup = require("GraphicGroup")
 local maths = require("maths")
 
 local map = maths.map
+local constrain = maths.constrain
 
-local thumbWidth = 12
+
+
+local MotionConstraint = {}
+local MotionConstraint_mt = {
+    __index = MotionConstraint
+}
+
+function MotionConstraint.new(self, obj)
+    obj = obj or {}
+    obj.minX = obj.minX or 0;
+    obj.minY = obj.minY or 0;
+    obj.maxX = obj.maxX or 1;
+    obj.maxY = obj.maxY or 1;
+
+    setmetatable(obj, MotionConstraint_mt)
+    
+    return obj;
+end
+
+function MotionConstraint.tryChange(self, subject, change)
+    --print("tryChange: ", change.dx, change.dy)
+    local x = constrain(subject.x+change.dx, self.minX, self.maxX)
+    local y = constrain(subject.y+change.dy, self.minY, self.maxY)
+    --print("tryChange, 2.0: ", x, y)
+
+    local dx = x - subject.x;
+    local dy = y - subject.y;
+
+    return {dx = dx,dy = dy}
+end
+
+
+--[[
+    Basic slider class
+]]
+local thumbWidth = 40
+local halfThumbWidth = thumbWidth/2
+
 local thumbHeight = 18
+local halfThumbHeight = thumbHeight/2
+local thumbRadius = 4;
+
 local trackThickness = 4
+
 
 local Slider = {}
 setmetatable(Slider, {
@@ -17,11 +59,20 @@ local Slider_mt = {
 
 function Slider.new(self, obj)
     obj = GraphicGroup:new(obj)
+
+    local yMidline = obj.frame.height/2
+
+    obj.bgColor = obj.bgColor or color(0xC0, 0xC0, 0xff)
     obj.lowValue = obj.lowValue or 0
     obj.highValue = obj.highValue or 255
     obj.position = obj.position or 0
     obj.dragging = false;
-    obj.thumbRect = BLRoundRect(-thumbWidth/2,obj.frame.height/2-thumbHeight/2,thumbWidth,thumbHeight,2,2)
+    obj.midline = yMidline;
+    obj.lastPosition = {x = halfThumbWidth, y = yMidline};
+    obj.thumbRect = BLRoundRect(0,yMidline-halfThumbHeight,thumbWidth,thumbHeight,thumbRadius, thumbRadius)
+    obj.constraint = MotionConstraint:new({
+        minX = 0, maxX = obj.frame.width-thumbWidth,
+        minY = obj.thumbRect.y, maxY = obj.thumbRect.y})
 
     setmetatable(obj,Slider_mt)
 
@@ -32,36 +83,51 @@ function Slider.draw(self, ctx)
     -- draw horizontal line mid height
     ctx:strokeWidth(trackThickness)
     ctx:stroke(120)
-    ctx:line(0,self.frame.height/2, self.frame.width, self.frame.height/2)
+    ctx:line(halfThumbWidth,self.midline, self.frame.width-halfThumbWidth, self.midline)
 
+---[[
     -- draw a couple of circles at the ends
     ctx:noStroke()
     ctx:fill(10)
-    ctx:circle(3, self.frame.height/2, 3)
-    ctx:circle(self.frame.width-3, self.frame.height/2, 3)
+    ctx:circle(halfThumbWidth, self.midline, trackThickness/2+2)
+    ctx:circle(self.frame.width-halfThumbWidth, self.frame.height/2, 3)
+--]]
 
     -- draw the thumb
     -- a lozinger rounded rect
-    ctx:fill(0xC0, 0xC0, 0xff)
+
+    ctx:fill(self.bgColor)
     ctx:fillRoundRect(self.thumbRect)
-
-    
 end
 
-function Slider.calcThumbPosition(self, x, y)
-    return map(x, 0, self.frame.width, 0, self.frame.width-thumbWidth)
+--[[
+    Returns a number in range  [0..1]
+]]
+function Slider.getPosition(self)
+    return map(self.thumbRect.x, self.constraint.minX, self.constraint.maxX, 0, 1)
 end
 
-function Slider.changeThumbPosition(self, x, y)
-    self.thumbRect.x = self:calcThumbPosition(event.x, event.y)
+function Slider.getValue(self)
+    return map(self:getPosition(), 0, 1, self.lowValue, self.highValue);
+end
+
+function Slider.changeThumbPosition(self, change)
+    local movement = self.constraint:tryChange(self.thumbRect, change)
+--print("movement: ", movement.dx, movement.dy)
+
+    self.thumbRect.x = self.thumbRect.x + movement.dx;
+    self.thumbRect.y = self.thumbRect.y + movement.dy
     
     -- tell anyone who's interested that something has changed
-    signalAll(self, self)
+    signalAll(self, self, "changeposition")
 end
 
 function Slider.mouseDown(self, event)
     self.dragging = true;
-    self:changeThumbPosition(event.x, event.y)
+    self.lastPosition = {x=event.x, y=event.y};
+    -- local change = {dx = centrx - event.x, dy = centery.event.y}
+    --self:changeThumbPosition({dy=0,dx=0})
+    --self:setThumbPosition()
 end
 
 function Slider.mouseUp(self, event)
@@ -70,9 +136,14 @@ end
 
 function Slider.mouseMove(self, event)
     --print("Slider.mouseMove: ", event.x, event.y, self.dragging)
+    local change = {
+        dy = event.y - self.lastPosition.y;
+        dx = event.x - self.lastPosition.x;    
+    }
     if self.dragging then 
-        self:changeThumbPosition(event.x, event.y)
+        self:changeThumbPosition(change)
     end
+    self.lastPosition = {x = event.x, y = event.y}
 end
 
 return Slider
--- testy/test_BEST_controls.lua
+++ testy/test_BEST_controls.lua
@@ -13,10 +13,24 @@ local desktopHeight = 768
 local function app(params)
     local win1 = WMCreateWindow(params)
 
-    local s1 = Slider:new({frame={x=20, y=20, width=320, height=20}})
+    local s1 = Slider:new({title = "red", bgColor = color(0xff,0,0), frame={x=20, y=20, width=256, height=20}})
+    local s2 = Slider:new({title = "green", bgColor = color(0,0xff,0), frame={x=20, y=44, width=256, height=20}})
+    local s3 = Slider:new({title = "blue", bgColor = color(0,0,0xff), frame={x=20, y=68, width=256, height=20}})
     
     win1:add(s1)
+    win1:add(s2)
+    win1:add(s3)
+
     win1:show()
+
+    local function handleSliderChange(slider, sig)
+        print(slider.title, sig, string.format("%32.f",slider:getPosition()), slider:getValue())
+    end
+
+    on(s1, handleSliderChange)
+    on(s2, handleSliderChange)
+    on(s3, handleSliderChange)
+
 
 ---[[
     while true do 
