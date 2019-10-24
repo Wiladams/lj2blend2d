d8dfd106efcf562f3b0b42c06d8a5c8f42defe7a
William Adams <Wiladams@users.noreply.github.com>
2019-10-24 10:40:59 -0700

    adding some 2d vector class

---
+++ testy/test_vec2.lua
@@ -0,0 +1,27 @@
+local dvec2 = require("vec2")
+
+local v1 = dvec2(1,2)
+local v2 = dvec2({3,4})
+
+print("v1: ", v1)
+print("v2: ", v2)
+
+local function test_add()
+    print("==== test_add ====")
+
+    print("v1 + v2: ", v1+v2)
+end
+
+local function test_sub()
+    print("==== test_sub ====")
+    print("v2 - v1: ", v2-v1)
+end
+
+local function test_negative()
+    print("==== test_negative ====")
+    print("-v1: ", -v1)
+end
+
+test_add()
+test_sub()
+test_negative()
